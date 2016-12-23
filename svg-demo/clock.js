5537ae8b9f64c935e82d6474eb3acbe41b5ac94a
xudong <xudong@xmind.net>
2016-12-23 10:13:04 +0800

    Complate clock animotion.

--- svg-demo/clock.js
+++ svg-demo/clock.js
@@ -1,4 +1,10 @@
 var clock;
+var secPerMinutes = 60;
+var secPerHour = 60*60;
+var secPer12Hour = 60 * 60 * 12;
+
+var hourHand , minute, seconds;
+
 function init() {
     clock = document.getElementById('clock');
     var svgns = clock.namespaceURI, doc = document;
@@ -24,7 +30,51 @@ function init() {
         ticks.appendChild(tickMark);
     }
 
-    var hands = clock.appendChild(document.createElementNS(svgns,'g'));
-    hands.style.cssText="stroke:black; "
+    var hands = clock.appendChild(doc.createElementNS(svgns,'g'));
+    hands.style.cssText="stroke:black; stroke-width: 5px; stroke-linecap:round;";
 
+    hourHand = hands.appendChild(doc.createElementNS(svgns, 'path'));
+    hourHand.id = 'hour';
+    hourHand.setAttribute('d', "M125,125 L125,75");
+    hourHand.setAttribute('transform',"rotate(0,125,125)");
+    hands.appendChild(hourHand);
+
+    minute = hands.appendChild(doc.createElementNS(svgns,'path'));
+    minute.id="minute";
+    minute.setAttribute('d',"M125,125 L125 65");
+    minute.setAttribute("transform", "rotate(0,125,125)");
+    hands.appendChild(minute);
+
+    seconds = hands.appendChild(doc.createElementNS(svgns, 'path'));
+    seconds.id="seconds";
+    seconds.setAttribute("d","M125, 125 L125, 55");
+    seconds.setAttribute("transform", "rotate(0, 125,125)");
+    hands.appendChild(seconds);
+    secondsTransform = seconds.getAttribute('transform');
+
+    var knob = doc.createElementNS(svgns, 'circle');
+    knob.setAttribute("cx","125");
+    knob.setAttribute("cy", "125");
+    knob.setAttribute("r","6");
+    clock.appendChild(knob);
+
+    updateClock();
+}
+
+function updateClock() {
+
+    var date = new Date();
+    var time = date.getMilliseconds()/1000+
+        date.getSeconds()+
+        date.getMinutes()*60+
+        date.getHours()*60*60;
+
+    var s = (360/secPerMinutes)*(time%secPerHour),
+        m = (360/secPerHour)*(time%secPerHour),
+        h = (360/secPer12Hour)*(time%secPer12Hour);
+    hourHand.setAttribute("transform","rotate("+h+",125,125)");
+    minute.setAttribute("transform", "rotate("+m+",125,125)");
+    seconds.setAttribute("transform","rotate("+s+", 125,125)");
+
+    window.requestAnimationFrame(updateClock);
 }
