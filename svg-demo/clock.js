f9033293978ec549d5415bf6e686084144958305
xudong <xudong@xmind.net>
2016-12-23 17:24:45 +0800

    add Transform and add drag for clock circle.

--- svg-demo/clock.js
+++ svg-demo/clock.js
@@ -1,9 +1,10 @@
-var clock;
+var clock,face;
 var secPerMinutes = 60;
-var secPerHour = 60*60;
+var secPerHour = 60 * 60;
 var secPer12Hour = 60 * 60 * 12;
+var beginDrag = false;
 
-var hourHand , minute, seconds;
+var hourHand, minute, seconds;
 
 function init() {
     clock = document.getElementById('clock');
@@ -11,7 +12,7 @@ function init() {
 
     clock.suspendRedraw(1000);
 
-    var face = doc.createElementNS(svgns, "circle");
+    face = doc.createElementNS(svgns, "circle");
     face.cx.baseVal.value = 125;
     face.cy.baseVal.value = 125;
     face.r.baseVal.value = 100;
@@ -30,51 +31,79 @@ function init() {
         ticks.appendChild(tickMark);
     }
 
-    var hands = clock.appendChild(doc.createElementNS(svgns,'g'));
-    hands.style.cssText="stroke:black; stroke-width: 5px; stroke-linecap:round;";
+    var hands = clock.appendChild(doc.createElementNS(svgns, 'g'));
+    hands.style.cssText = "stroke:black; stroke-width: 5px; stroke-linecap:round;";
 
     hourHand = hands.appendChild(doc.createElementNS(svgns, 'path'));
     hourHand.id = 'hour';
     hourHand.setAttribute('d', "M125,125 L125,75");
-    hourHand.setAttribute('transform',"rotate(0,125,125)");
+    hourHand.setAttribute('transform', "rotate(0,125,125)");
     hands.appendChild(hourHand);
 
-    minute = hands.appendChild(doc.createElementNS(svgns,'path'));
-    minute.id="minute";
-    minute.setAttribute('d',"M125,125 L125 65");
+    minute = hands.appendChild(doc.createElementNS(svgns, 'path'));
+    minute.id = "minute";
+    minute.setAttribute('d', "M125,125 L125 65");
     minute.setAttribute("transform", "rotate(0,125,125)");
     hands.appendChild(minute);
 
     seconds = hands.appendChild(doc.createElementNS(svgns, 'path'));
-    seconds.id="seconds";
-    seconds.setAttribute("d","M125, 125 L125, 55");
+    seconds.id = "seconds";
+    seconds.setAttribute("d", "M125, 125 L125, 55");
     seconds.setAttribute("transform", "rotate(0, 125,125)");
     hands.appendChild(seconds);
     secondsTransform = seconds.getAttribute('transform');
 
     var knob = doc.createElementNS(svgns, 'circle');
-    knob.setAttribute("cx","125");
+    knob.setAttribute("cx", "125");
     knob.setAttribute("cy", "125");
-    knob.setAttribute("r","6");
+    knob.setAttribute("r", "6");
     clock.appendChild(knob);
 
     updateClock();
+    addDragForClock();
 }
 
 function updateClock() {
 
     var date = new Date();
-    var time = date.getMilliseconds()/1000+
-        date.getSeconds()+
-        date.getMinutes()*60+
-        date.getHours()*60*60;
+    var time = date.getMilliseconds() / 1000 +
+        date.getSeconds() +
+        date.getMinutes() * 60 +
+        date.getHours() * 60 * 60;
 
-    var s = (360/secPerMinutes)*(time%secPerHour),
-        m = (360/secPerHour)*(time%secPerHour),
-        h = (360/secPer12Hour)*(time%secPer12Hour);
-    hourHand.setAttribute("transform","rotate("+h+",125,125)");
-    minute.setAttribute("transform", "rotate("+m+",125,125)");
-    seconds.setAttribute("transform","rotate("+s+", 125,125)");
+    var s = (360 / secPerMinutes) * (time % secPerHour),
+        m = (360 / secPerHour) * (time % secPerHour),
+        h = (360 / secPer12Hour) * (time % secPer12Hour);
+    hourHand.setAttribute("transform", "rotate(" + h + ",125,125)");
+    minute.setAttribute("transform", "rotate(" + m + ",125,125)");
+    seconds.setAttribute("transform", "rotate(" + s + ", 125,125)");
 
     window.requestAnimationFrame(updateClock);
+}
+
+function addDragForClock() {
+    face.addEventListener("mousedown", onDragStart, false);
+    face.addEventListener('mousemove', doMoveClock, false);
+    face.addEventListener("mouseup", onDragEnd, false);
+}
+
+function onDragStart(evt) {
+
+    // evt.target.getAttribute('id');
+    onDragEnd(evt);
+    beginDrag = true;
+}
+
+function doMoveClock(evt) {
+    if (!beginDrag)
+        return;
+
+    var x = evt.clientX;
+    var y = evt.clientY;
+
+    face.setAttribute("transform", "translate(" + (x-125) + "," + (y-125) + ")");
+}
+
+function onDragEnd(evt) {
+    beginDrag = false;
 }
