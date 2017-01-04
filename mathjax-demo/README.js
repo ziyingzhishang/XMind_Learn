6500a534ed742849d2f26824fd3c2fda942deb2a
xudong <xudong@xmind.net>
2017-01-04 08:21:04 +0800

    add svg for mathjax demo

---
+++ mathjax-demo/README.js
@@ -0,0 +1,14 @@
+// 缺点
+// 使用mathjax，插入inline公式比较困难一点；
+// 库比较大，经过精简应该可以解决；
+// 生成公式过程中，如果输入latex，需要输入完成标签，比mathquill稍微差点，不过结合mathquill可以通过修改源码解决；
+
+
+// 优点
+// 可以生成svg公式；
+// 通过配置，操作简单；
+// 提供很多mathjax的CDN；
+// 支持多种渲染方式，mathml, svg, html+css , latex, tex, 
+
+
+// 采用mathquill的编辑方式，采用mathjax的渲染方式，两者结合一下应该就比较完美了
