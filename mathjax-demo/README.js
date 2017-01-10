9baa776d1f9760bcaf964636a343daee9bd6e913
xudong <xudong@xmind.net>
2017-01-10 17:43:47 +0800

    Complete Math insert in svg.

--- mathjax-demo/README.js
+++ mathjax-demo/README.js
@@ -1,5 +1,6 @@
-// 缺点
-// 使用mathjax，插入inline公式比较困难一点；
+// MathJax的优缺点：
+// 缺点：
+// 插入inline公式比较困难一点；
 // 库比较大，经过精简应该可以解决；
 // 生成公式过程中，如果输入latex，需要输入完成标签，比mathquill稍微差点，不过结合mathquill可以通过修改源码解决；
 
@@ -10,5 +11,38 @@
 // 提供很多mathjax的CDN；
 // 支持多种渲染方式，mathml, svg, html+css , latex, tex, 
 
+// 解决方案：
+// 使用mathquill进行公式编辑，使用mathjax将mathquill编辑之后的latex标签渲染成svg插入到topic中；
 
-// 采用mathquill的编辑方式，采用mathjax的渲染方式，两者结合一下应该就比较完美了
+// 待解决问题：
+// mathquill 依赖jquery，还有自己的css, 而mathjax库也比较大，进行公示编辑的话，会在页面中引入较多的外部文件
+// 影响页面响应速度；
+// 在group中插入svg图片后的定位问题；（目测较容易解决）；
+// 渲染时机的选择，类似文字处理，当编辑窗失去焦点进行svg渲染；（容易解决）；
+// inline问题；
+// 字体问题；
+
+// 尝试将mathjax裁剪，只选择渲染svg的部分；
+
+
+
+
+// -----------------------------------------
+/**
+ * Mathquill 依赖：
+ * "pjs": ">=3.1.0 <5.0.0",
+    "mocha": ">=2.4.1",
+    "uglify-js": "2.x",
+    "less": ">=1.5.1
+    MathQuill license：MPL-2.0,
+ * 
+ * Mathjax ： 原生库176M，没有依赖，
+   Mathjax-node ：依赖库：
+    "jsdom": "7.*",
+    "mathjax": "*",
+    "speech-rule-engine": "*",
+    "yargs": "3.*"
+   
+   Mathjax license： Apache 2.0
+
+ */
---
+++ mathjax-demo/sample-dynamic-2.js
@@ -0,0 +1,227 @@
+/**
+ * 1. 为body中的元素添加拖拽；
+ * 2. 绘制svg；
+ * 3. 设置公式编辑；
+ * 4. 将生成的svg加入到原来的svg节点中；
+ * ----------------------------------、
+ * 1. 设置css ，在svg元素上进行公式编辑；
+ * 2. 结合mathquill进行公式编辑；
+ * 3. 模拟动态插入公式topic节点；
+ * 4. svg的定位问题（使用transform 解决： transform）
+ */
+
+var rDrag = {
+
+    o: null,
+
+    init: function (o) {
+        o.onmousedown = this.start;
+        o.onkeydown = this.zoom;
+        o.oldScale = 1.0;
+    },
+    start: function (e) {
+        var o;
+        e = rDrag.fixEvent(e);
+        e.preventDefault && e.preventDefault();
+        rDrag.o = o = this;
+        o.x = e.clientX - rDrag.o.offsetLeft;
+        o.y = e.clientY - rDrag.o.offsetTop;
+        document.onmousemove = rDrag.move;
+        document.onmouseup = rDrag.end;
+    },
+    zoom: function (e) {
+        var o;
+        e = rDrag.fixEvent(e);
+        // e.preventDefault && e.preventDefault();
+        rDrag.o = o = this;
+        o.oldScale = rDrag.o.oldScale;
+        o.bCtrlKey = e.ctrlKey;
+        switch (e.keyCode) {
+            case 38:
+                rDrag.zoomin(e);
+                break;
+            case 40:
+                rDrag.zoomout(e);
+                break;
+        }
+    },
+    zoomin: function (e) {
+        if (!rDrag.o.bCtrlKey)
+            return;
+        rDrag.o.oldScale = rDrag.o.oldScale + 0.1;
+        rDrag.o.style.transform = 'scale(' + (rDrag.o.oldScale) + ')';
+    },
+    zoomout: function (e) {
+        if (!rDrag.o.bCtrlKey)
+            return;
+        rDrag.o.oldScale = rDrag.o.oldScale - 0.1;
+        rDrag.o.style.transform = 'scale(' + (rDrag.o.oldScale) + ')';
+
+    },
+    move: function (e) {
+        e = rDrag.fixEvent(e);
+        var oLeft, oTop;
+        oLeft = e.clientX - rDrag.o.x;
+        oTop = e.clientY - rDrag.o.y;
+        rDrag.o.style.left = oLeft + 'px';
+        rDrag.o.style.top = oTop + 'px';
+    },
+    end: function (e) {
+        e = rDrag.fixEvent(e);
+        rDrag.o = document.onmousemove = document.onmouseup = null;
+    },
+    fixEvent: function (e) {
+        if (!e) {
+            e = window.event;
+            e.target = e.srcElement;
+            e.layerX = e.offsetX;
+            e.layerY = e.offsetY;
+        }
+        return e;
+    }
+}
+
+var wmargin = 10, hmargin = 5;
+var group, draw;
+window.onload = function() {
+    var mathSpan = document.getElementById("body");
+    var w = mathSpan.offsetWidth, h = mathSpan.offsetHeight;
+
+    var body = document.getElementById("body");
+    var outw = body.offsetWidth, outh = body.offsetHeight;
+
+    draw = SVG('svg').size(outw, outh);
+    rect = draw.rect(10, 10).attr({ fill: "lightblue", stroke: "black" });
+    group = draw.group();
+    var centerdiv = document.getElementById('svg');
+    rDrag.init(centerdiv);
+    // var w = 
+    // resizeSvg(rect, w, h);
+
+    var problemSpan = document.getElementById('problem');
+    // var math = MQ.StaticMath(problemSpan);
+    // console.log(math.latex());
+    var proMathField = MQ.MathField(problemSpan, {
+        handlers: {
+            edit: function () {
+                var enteredMath = proMathField.latex(); // Get entered math in LaTeX format
+                var w = problemSpan.offsetWidth, h = problemSpan.offsetHeight;
+                // checkAnswer(enteredMath);
+                document.getElementById("MathInput").value = '$'+enteredMath+'$'
+                Preview.Update();
+                resizeSvg(rect, w, h);
+                // console.log(enteredMath);
+            }
+        }
+    });
+}
+
+function resizeSvg(target, w = 0, h = 0) {
+    if (w != 0)
+        target.width(w + 2 * wmargin);
+    if (h != 0)
+        target.height(h + 2 * hmargin);
+}
+
+
+
+var Preview = {
+  delay: 150,        // delay after keystroke before updating
+
+  preview: null,     // filled in by Init below
+  buffer: null,      // filled in by Init below
+
+  timeout: null,     // store setTimout id
+  mjRunning: false,  // true when MathJax is processing
+  mjPending: false,  // true when a typeset has been queued
+  oldText: null,     // used to check if an update is needed
+
+  //
+  //  Get the preview and buffer DIV's
+  //
+  Init: function () {
+    this.preview = document.getElementById("MathPreview");
+    this.buffer = document.getElementById("MathBuffer");
+  },
+
+  //
+  //  Switch the buffer and preview, and display the right one.
+  //  (We use visibility:hidden rather than display:none since
+  //  the results of running MathJax are more accurate that way.)
+  //
+  SwapBuffers: function () {
+    var buffer = this.preview, preview = this.buffer;
+    this.buffer = buffer; this.preview = preview;
+    buffer.style.visibility = "hidden"; buffer.style.position = "absolute";
+    preview.style.position = ""; preview.style.visibility = "";
+
+    var e = preview.getElementsByTagName("svg");
+    var image;
+    if(e.length != 0){
+      var pNode = group.node;
+      pNode.setAttribute("transform","translate("+wmargin+","+hmargin+")");
+      var old = pNode.getElementsByTagName('svg')
+      if(old.length === 1){
+        pNode.removeChild(old[0]);
+      }
+      e[0].y = hmargin;
+      var w = e[0].getBoundingClientRect().width, h = e[0].getBoundingClientRect().height;
+      pNode.appendChild(e[0]);
+      resizeSvg(rect, w, h);
+    }
+  },
+
+  //
+  //  This gets called when a key is pressed in the textarea.
+  //  We check if there is already a pending update and clear it if so.
+  //  Then set up an update to occur after a small delay (so if more keys
+  //    are pressed, the update won't occur until after there has been 
+  //    a pause in the typing).
+  //  The callback function is set up below, after the Preview object is set up.
+  //
+  Update: function () {
+    if (this.timeout) {clearTimeout(this.timeout)}
+    this.timeout = setTimeout(this.callback,this.delay);
+  },
+
+  //
+  //  Creates the preview and runs MathJax on it.
+  //  If MathJax is already trying to render the code, return
+  //  If the text hasn't changed, return
+  //  Otherwise, indicate that MathJax is running, and start the
+  //    typesetting.  After it is done, call PreviewDone.
+  //  
+  CreatePreview: function () {
+    Preview.timeout = null;
+    if (this.mjPending) return;
+    var text = document.getElementById("MathInput").value;
+    if (text === this.oldtext) return;
+    if (this.mjRunning) {
+      this.mjPending = true;
+      MathJax.Hub.Queue(["CreatePreview",this]);
+    } else {
+      this.buffer.innerHTML = this.oldtext = text;
+      this.mjRunning = true;
+      MathJax.Hub.Queue(
+        ["Typeset",MathJax.Hub,this.buffer],
+        ["PreviewDone",this]
+      );
+    }
+  },
+
+  //
+  //  Indicate that MathJax is no longer running,
+  //  and swap the buffers to show the results.
+  //
+  PreviewDone: function () {
+    this.mjRunning = this.mjPending = false;
+    this.SwapBuffers();
+  }
+
+};
+
+//
+//  Cache a callback to the CreatePreview action
+//
+Preview.callback = MathJax.Callback(["CreatePreview",Preview]);
+Preview.callback.autoReset = true;  // make sure it can run more than once
